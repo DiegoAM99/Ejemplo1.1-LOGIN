5247c3afdeb69e8f1922b9ade7c9ef7ad85e75b9
Administrator <Administrator@192.168.145.130>
2018-10-08 19:23:45 +0200

    Esta a punto!!!!!

--- juego.php
+++ juego.php
@@ -10,5 +10,13 @@ $tema = $_POST['tema'];
 <div>
     <p></p>
     <p><a class="btn btn-block btn-dark disabled">Demuestra que estás listo para la EVAU</a></p>
+    <p><a class="btn btn-block btn-warning" onclick="volver();">Volver al Menú</a></p>
     <p><a id="sigue1" class="btn btn-block btn-primary"><?php echo $tema;?></a></p>
+    
 </div>
+
+<script>
+    function volver(){
+       $('#principal').load('app.php');
+    }    
+</script>
