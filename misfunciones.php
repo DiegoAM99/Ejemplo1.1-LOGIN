bc730dc246dfd0a55e62f36d6fe1aabc698624ef
Administrator <Administrator@192.168.145.130>
2018-10-09 18:40:50 +0200

    Esta casi!!!

--- misfunciones.php
+++ misfunciones.php
@@ -3,7 +3,7 @@
 function conectaBBDD(){
     require ('configuracion.php');
     $mysqli = new mysqli($servidor,$usuario_mysql, $clave_mysql, $bbdd);
-    $mysqli ->query("SET NAMES utf8");
+    $mysqli -> query("SET NAMES utf8");
     return $mysqli;
 }
 
